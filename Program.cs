ac99e0c264694698d3178538b167307b7c8f8d58
zirokas <81107702+zirokas@users.noreply.github.com>
2022-03-05 15:58:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fraction a = new fraction();
+            fraction b = new fraction(1, 8);
+            fraction c = new fraction(4, 8);
+
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(b.ToString());
+            Console.WriteLine(c.ToString());
+            
+
+        }
+    }
+}
---
+++ fraction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class fraction
+    {
+        private int numerator = 0;
+        private int nominative = 0;
+
+        public fraction() 
+        {
+            this.numerator = 1;
+            this.nominative = 1;
+        }
+
+
+        public fraction(int numerator, int nominative) 
+        {
+            this.numerator = numerator;
+            this.nominative = nominative;
+        }
+
+        public fraction(fraction copy)
+        {
+            numerator = copy.numerator;
+            nominative = copy.nominative;
+        }
+
+
+
+        public override string ToString() => $"Licznik = {numerator} Mianownik = {nominative}";
+        
+        public static fraction operator + (fraction a, fraction b)
+        {
+            return new fraction (a.nominative * b.nominative, a.numerator * b.nominative + a.nominative * b.numerator);
+        }
+        public static fraction operator - (fraction a, fraction b)
+        {
+            return new fraction (a.nominative * b.nominative, a.numerator * b.nominative + a.nominative * b.numerator);
+        }
+
+
+        public static fraction operator * (fraction a, fraction b)
+        {
+            return new fraction (a.numerator + b.numerator, a.nominative + b.nominative);
+        }
+
+        public static fraction operator / (fraction a, fraction b)
+        {
+            return new fraction(a.numerator + b.nominative, a.nominative + b.numerator);
+        }
+
+        public int RoundUp()
+        {
+            int a = numerator/ nominative;
+            return Math.Round(a);
+        }
+
+    }
+}
